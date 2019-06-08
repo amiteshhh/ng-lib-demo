d4d66db5e85a3bf1d3b00029ca7b1a1ec386c7e1
Amitesh Kumar <amitesh_kumar@epam.com>
2019-06-08 17:39:11 +0530

    readme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import {MatButtonModule} from '@angular/material'
-import {DealModule} from 'nglibdeal';
+import { MatButtonModule } from '@angular/material'
+import { DealModule } from 'nglibdeal';
 
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -11,10 +13,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     MatButtonModule,
-    DealModule.forRoot('DEV')
+    DealModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor() {
+    var libConfigs = [DealModule.configEnv];//keep adding other modules
+    libConfigs.forEach(libConfig => libConfig(environment.envCode))
+  }
+}
